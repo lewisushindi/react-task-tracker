8412c6914d1a599747f17c1a1ac42f5dd4514ffd
Lewis ushindi <coderflame3@gmail.com>
2023-03-07 17:54:10 +0300

    Adding Button component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import Button from "./components/Button";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="container">        
+    <div className="container">
+      <div className="header">
         <Header />
+        <Button />
+      </div>      
+        
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,8 @@
 
 const Header = ({title}) => {
   return (
-    <header className="header">
-        <h1>{title}</h1>
-        <button className="btn">Add</button>
+    <header>
+        <h1>{title}</h1>        
     </header>
   )
 }
