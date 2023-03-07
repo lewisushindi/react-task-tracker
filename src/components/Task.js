66990a30228fe067dd5ffe7c2aa0782d38d67292
Lewis ushindi <coderflame3@gmail.com>
2023-03-07 18:30:02 +0300

    Adding sample task Array

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,4 +1,20 @@
-
+const task =[
+    {
+        id: 1,
+        text: 'Learn Wp plugin Development',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Learn React.Js',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Build Projects',
+        reminder: true,
+    },
+]
 
 const Task = () => {
   return (
